7e6502bedfd0d1784fde25cbe5b8fdf7c97adc45
sachinopboy2 <sachin25u7@gmail.com>
2026-02-06 18:49:13 +0530

    Update zowner.py

--- BrandrdXMusic/plugins/tools/zowner.py
+++ BrandrdXMusic/plugins/tools/zowner.py
@@ -12,12 +12,12 @@ from BrandrdXMusic.utils.database import add_served_chat, get_assistant
 async def help(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://telegra.ph/file/1aac9a42f6f35138da34b.jpg",
-        caption=f"""🍁𝐂𝐋𝐈𝐂𝐊🥰𝐁𝐄𝐋𝐎𝐖💝𝐁𝐔𝐓𝐓𝐎𝐍✨𝐓𝐎🙊𝐆𝐄𝐓🌱𝐑𝐄𝐏𝐎🍁""",
+        caption=f""" ⎯⁠⁠⁠⁠‌⎯⁠⁠⁠‌ ꯭꯭𝚴꯭‌𝛐꯭𝛃꯭‌𝛊꯭‌𝛕꯭𝛂꯭‌𝅃⁠⁠⁠ꀭ꯭‧₊꯭♡゙꯭꯬ """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "ƨσʋяcɛ", url=f"https://github.com/WCGKING/BrandrdXMusic"
+                        "⎯⁠⁠⁠⁠‌⎯⁠⁠⁠‌ ꯭꯭𝚴꯭‌𝛐꯭𝛃꯭‌𝛊꯭‌𝛕꯭𝛂꯭‌𝅃⁠⁠⁠ꀭ꯭‧₊꯭♡゙꯭꯬", url=f"https://t.me/NOBITAXD7"
                     )
                 ]
             ]
@@ -34,7 +34,7 @@ async def clones(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "ƨσʋяcɛ", url=f"https://github.com/WCGKING/BrandrdXMusic"
+                        "⎯⁠⁠⁠⁠‌⎯⁠⁠⁠‌ ꯭꯭𝚴꯭‌𝛐꯭𝛃꯭‌𝛊꯭‌𝛕꯭𝛂꯭‌𝅃⁠⁠⁠ꀭ꯭‧₊꯭♡゙꯭꯬", url=f"https://t.me/NOBITAXD7"
                     )
                 ]
             ]
