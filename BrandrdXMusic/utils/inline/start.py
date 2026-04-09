8ae8f7ee774fd48173b54e16baf83f9702bde6d7
sachinopboy2 <sachin25u7@gmail.com>
2026-04-09 14:54:47 +0530

    Update start.py

--- BrandrdXMusic/utils/inline/start.py
+++ BrandrdXMusic/utils/inline/start.py
@@ -1,22 +1,21 @@
 import config
 from BrandrdXMusic import app
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from pyrogram.enums import ButtonStyle
+
+# Yahan se ButtonStyle wala import hata diya gaya hai
 
 def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
                 text="🟢 " + _["S_B_1"],
-                url=f"https://t.me/{app.username}?startgroup=true",
-                style=ButtonStyle.SUCCESS, # Green
+                url=f"https://t.me/{app.username}?startgroup=true"
             )
         ],
         [
             InlineKeyboardButton(
                 text="🔷 " + _["S_B_2"],
-                url=config.SUPPORT_CHAT,
-                style=ButtonStyle.PRIMARY, # Blue
+                url=config.SUPPORT_CHAT
             ),
         ],
     ]
@@ -28,34 +27,29 @@ def private_panel(_):
         [
             InlineKeyboardButton(
                 text="🟢 " + _["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
-                style=ButtonStyle.SUCCESS, # Green
+                url=f"https://t.me/{app.username}?startgroup=true"
             )
         ],
         [
             InlineKeyboardButton(
                 text="⚙️ " + _["S_B_4"],
-                callback_data="settings_back_helper",
-                style=ButtonStyle.PRIMARY, # Blue
+                callback_data="settings_back_helper"
             )
         ],
         [
             InlineKeyboardButton(
                 text="👑 " + _["S_B_5"],
-                url=f"tg://user?id={config.OWNER_ID}", # user_id ki jagah direct url link
-                style=ButtonStyle.PRIMARY,
+                url=f"tg://user?id={config.OWNER_ID}"
             ),
             InlineKeyboardButton(
                 text="💬 " + _["S_B_2"],
-                url=config.SUPPORT_CHAT,
-                style=ButtonStyle.PRIMARY,
+                url=config.SUPPORT_CHAT
             ),
         ],
         [
             InlineKeyboardButton(
                 text="🔴 " + _["S_B_6"],
-                url=config.SUPPORT_CHANNEL,
-                style=ButtonStyle.DANGER, # Red
+                url=config.SUPPORT_CHANNEL
             ),
         ],
     ]
