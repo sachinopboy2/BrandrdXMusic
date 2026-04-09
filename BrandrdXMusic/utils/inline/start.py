3ae5e9a14cb87923f5ee62a84c3f0b9eb45e638b
sachinopboy2 <sachin25u7@gmail.com>
2026-04-09 14:51:36 +0530

    Update start.py

--- BrandrdXMusic/utils/inline/start.py
+++ BrandrdXMusic/utils/inline/start.py
@@ -1,62 +1,63 @@
 import config
 from BrandrdXMusic import app
-from BrandrdXMusic.utils.inline.button import ikb, ButtonStyle
-
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.enums import ButtonStyle
 
 def start_panel(_):
     buttons = [
         [
-            ikb(
+            InlineKeyboardButton(
                 text="🟢 " + _["S_B_1"],
                 url=f"https://t.me/{app.username}?startgroup=true",
-                style=ButtonStyle.SUCCESS,
+                style=ButtonStyle.SUCCESS, # Green
             )
         ],
         [
-            ikb(
+            InlineKeyboardButton(
                 text="🔷 " + _["S_B_2"],
                 url=config.SUPPORT_CHAT,
-                style=ButtonStyle.PRIMARY,
+                style=ButtonStyle.PRIMARY, # Blue
             ),
         ],
     ]
-    return buttons
+    return InlineKeyboardMarkup(buttons)
 
 
 def private_panel(_):
     buttons = [
         [
-            ikb(
+            InlineKeyboardButton(
                 text="🟢 " + _["S_B_3"],
                 url=f"https://t.me/{app.username}?startgroup=true",
-                style=ButtonStyle.SUCCESS,
+                style=ButtonStyle.SUCCESS, # Green
             )
         ],
         [
-            ikb(
+            InlineKeyboardButton(
                 text="⚙️ " + _["S_B_4"],
                 callback_data="settings_back_helper",
-                style=ButtonStyle.PRIMARY,
+                style=ButtonStyle.PRIMARY, # Blue
             )
         ],
         [
-            ikb(
+            InlineKeyboardButton(
                 text="👑 " + _["S_B_5"],
-                user_id=config.OWNER_ID,
+                url=f"tg://user?id={config.OWNER_ID}", # user_id ki jagah direct url link
                 style=ButtonStyle.PRIMARY,
             ),
-            ikb(
+            InlineKeyboardButton(
                 text="💬 " + _["S_B_2"],
                 url=config.SUPPORT_CHAT,
                 style=ButtonStyle.PRIMARY,
             ),
         ],
         [
-            ikb(
+            InlineKeyboardButton(
                 text="🔴 " + _["S_B_6"],
                 url=config.SUPPORT_CHANNEL,
-                style=ButtonStyle.DANGER,
+                style=ButtonStyle.DANGER, # Red
             ),
         ],
     ]
-    return buttons
+    return InlineKeyboardMarkup(buttons)
+    
