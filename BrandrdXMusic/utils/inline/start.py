15dee8bdee228993fc2d26350f6381aa8ec783fb
sachinopboy2 <sachin25u7@gmail.com>
2026-04-09 14:40:26 +0530

    Update start.py

--- BrandrdXMusic/utils/inline/start.py
+++ BrandrdXMusic/utils/inline/start.py
@@ -2,16 +2,19 @@ import config
 from BrandrdXMusic import app
 from BrandrdXMusic.utils.inline.button import ikb, ButtonStyle
 
+
 def start_panel(_):
     buttons = [
         [
             ikb(
-                text=_["S_B_1"],
+                text="🟢 " + _["S_B_1"],
                 url=f"https://t.me/{app.username}?startgroup=true",
                 style=ButtonStyle.SUCCESS,
-            ),
+            )
+        ],
+        [
             ikb(
-                text=_["S_B_2"],
+                text="🔷 " + _["S_B_2"],
                 url=config.SUPPORT_CHAT,
                 style=ButtonStyle.PRIMARY,
             ),
@@ -24,33 +27,33 @@ def private_panel(_):
     buttons = [
         [
             ikb(
-                text=_["S_B_3"],
+                text="🟢 " + _["S_B_3"],
                 url=f"https://t.me/{app.username}?startgroup=true",
                 style=ButtonStyle.SUCCESS,
             )
         ],
         [
             ikb(
-                text=_["S_B_4"],
+                text="⚙️ " + _["S_B_4"],
                 callback_data="settings_back_helper",
                 style=ButtonStyle.PRIMARY,
             )
         ],
         [
             ikb(
-                text=_["S_B_5"],
+                text="👑 " + _["S_B_5"],
                 user_id=config.OWNER_ID,
                 style=ButtonStyle.PRIMARY,
             ),
             ikb(
-                text=_["S_B_2"],
+                text="💬 " + _["S_B_2"],
                 url=config.SUPPORT_CHAT,
                 style=ButtonStyle.PRIMARY,
             ),
         ],
         [
             ikb(
-                text=_["S_B_6"],
+                text="🔴 " + _["S_B_6"],
                 url=config.SUPPORT_CHANNEL,
                 style=ButtonStyle.DANGER,
             ),
